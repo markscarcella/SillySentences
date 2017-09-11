7ec9db64ebce20338f3ce796e35d19edf9810c44
meridiantaal <mark.scarcella@gmail.com>
2017-09-12 00:43:00 +1000

    Empty (mostly) version

---
+++ sillySentences_activity.py
@@ -0,0 +1,50 @@
+# Import Tkinter and the image packages
+from Tkinter import *
+from PIL import ImageTk, Image
+# Import the random package
+import random
+
+# Make a function to generate a random sentance
+def generateSentance(outputText):
+	# define lists of adjectives, nouns, verbs, and adverbs
+	adjective = []
+	noun = []
+	verb = []
+	adverb = []
+	# construct a sentance using random words from the lists above
+
+	# take the output text widget and delete all the characters
+
+	# insert the sentance into the output text
+
+
+# Make your Tkinter window
+window = Tk()
+# Give it a title
+
+# Set it's geometry
+
+# Open an image and resize it
+
+# Make a background label with the image in it
+
+# Place the background at (0,0)
+
+
+# Make a credits label
+
+# pack the label at the bottom, filling across in X
+
+
+# Make a text widget to display the output
+
+# pack the output text widget
+
+
+# Make a button to generate a sentance when you click
+
+# pack the button widget
+
+
+# Start the main loop
+window.mainloop()
